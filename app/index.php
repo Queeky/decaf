738b3dcb4cc317dfd4a498f0022d53b7031e06de
Queeky <qamy.ava@gmail.com>
2024-01-09 23:05:04 -0600

    Initial push

---
+++ app/index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Homepage</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <header>
+            <form action="index.php" method="GET">
+                <input class='search-bar' type="text">
+                <button type='submit'>Search</button>
+            </form>
+        </header>
+        <nav>
+            <ul>
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">ABOUT</a></li>
+                <li><a href="#">MADLIBS</a></li>
+                <li><a href="#">1-WORD</a></li>
+                <li><a href="#">COLORING BOOK</a></li>
+            </ul>
+        </nav>
+        <div class='full-page'>
+            <div class='left-bar'>
+                <div>
+                    <img src="images/lord_lobster.png" alt="">
+                </div>
+                <div>
+                    <img src="images/lord_lobster.png" alt="">
+                </div>
+                <div>
+                    <img src="images/lord_lobster.png" alt="">
+                </div>
+            </div>
+            <div class='content'>
+                <div class='splash'>
+                    <p>I put the za in <a target="_blank" href="https://zamboni.com/product/model-552ac/">Model 522AC Zamboni</a>.</p>
+                </div>
+                <div class='inner-content'>
+                    <div class='main'>
+                        <div>
+                            <h3>LATEST STORY</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Orci eu lobortis elementum nibh tellus. Orci sagittis eu volutpat odio. Purus sit amet luctus venenatis lectus magna fringilla. Aenean sed adipiscing diam donec adipiscing tristique risus nec. Leo integer malesuada nunc vel risus commodo viverra maecenas accumsan. Felis bibendum ut tristique et. Justo laoreet sit amet cursus sit amet dictum sit. Porttitor massa id neque aliquam vestibulum morbi blandit cursus risus. Vel elit scelerisque mauris pellentesque. Faucibus purus in massa tempor nec feugiat nisl pretium fusce. Sit amet venenatis urna cursus eget nunc scelerisque viverra. Sapien eget mi </p>
+                        </div>
+                    </div>
+                    <div class='side'>
+                        <div>
+                            <h3>WORD OF THE DAY</h3>
+                            <p class='word'>LYCANTHROPY</p>
+                            <p class='word-type'>noun</p>
+                            <p class='word-def'>A delusion that one has become a wolf; the assumption of the form and characteristics of a wolf held to be possible by witchcraft or magic.</p>
+                            <ul class='synonyms'>
+                                <p>Synonyms:</p>
+                                <li><a href="#">zoanthropy</a></li>
+                                <li><a href="#">zoanthropy</a></li>
+                                <li><a href="#">zoanthropy</a></li>
+                                <li><a href="#">zoanthropy</a></li>
+                            </ul>
+                            <ul class='antonyms'>
+                                <p>Antonyms:</p>
+                                <li><a href="#">zoanthropy</a></li>
+                                <li><a href="#">zoanthropy</a></li>
+                                <li><a href="#">zoanthropy</a></li>
+                                <li><a href="#">zoanthropy</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class='right-bar'>
+                <ul class='links'>
+                    <li><a href="https://classicreload.com/the-lost-treasures-of-infocom-volume-i.html">The Lost Treasures of Infocom</a></li>
+                    <li><a href="https://archive.org/">Internet Archive</a></li>
+                    <li><a href="https://libgen.li/">Library Genesis</a></li>
+                </ul>
+            </div>
+        </div>
+        <footer>
+            Hey there
+        </footer>
+    </body>
+</html>
